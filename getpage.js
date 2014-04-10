ab55d7e031de72bb0cd11fd0f473a8b0ccbf7c6e
guci <guci314@gmail.com>
2014-04-10 17:45:05 +0800

    add xuefo

--- getpage.js
+++ getpage.js
@@ -8,9 +8,16 @@ var cheerio = require('cheerio');
 //var dom = require('xmldom').DOMParser;
 var tidy = require('htmltidy').tidy;
 //var parser = require('libxml-to-js');
-var u = 'http://www.qqyy.com/jibing/xhnk/131009/4c363.html';
+//qqyy
+//var qqyy = 'http://www.qqyy.com/jibing/xhnk/131009/4c363.html';
+//var path = 'html body div div';//table tbody tr td table#table2 tbody tr td html body div div
+//var s = $(path).attr('style', 'color:#333;').text();
+
+var xuefo='http://www.xuefo.net/nr/article20/198170.html';
+
 var gate = 'http://gate.baidu.com/tc?bd_page_type=1&src=';
-var url = gate + u;
+var url = gate + xuefo;
+
 // The structure of our request call
 // The first parameter is our URL
 // The callback function takes 3 parameters, an error, response status code and the html
@@ -24,8 +31,8 @@ request(url, function (error, response, html) {
         var dowork = function (err, html1) {
             //console.log(html1);
             var $ = cheerio.load(html1);
-            var path = 'html body div div';//table tbody tr td table#table2 tbody tr td html body div div
-            var s = $(path).attr('style', 'color:#333;').text();
+            var path = 'html body div div div.c1 div.c2';//table tbody tr td table#table2 tbody tr td html body div div
+            var s = $(path).text();
             console.log(s);
 //            $(path).each(function(i, elem) {
 //                console.log('aaaaaaaaaaaaaa');
