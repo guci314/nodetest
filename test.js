b062d8e93aad8924292673979cd4188805eb4faf
guci <guci314@gmail.com>
2014-04-10 22:47:27 +0800

    add test.js

---
+++ test.js
@@ -0,0 +1,18 @@
+var terminal = require('child_process').spawn('bash');
+
+terminal.stdout.on('data', function (data) {
+    console.log('stdout: ' + data);
+});
+
+terminal.on('exit', function (code) {
+    console.log('child process exited with code ' + code);
+});
+
+setTimeout(function() {
+    console.log('Sending stdin to terminal');
+    terminal.stdin.write('echo "Hello $USER. Your machine runs since:"\n');
+    terminal.stdin.write('uptime\n');
+    terminal.stdin.write('ls\n');
+    console.log('Ending terminal session');
+    terminal.stdin.end();
+}, 1000);
